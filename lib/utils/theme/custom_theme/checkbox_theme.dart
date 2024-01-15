a81bc12372c4648b848d647e8f40314b5f1f04ba
Praveen Maurya <112618229+praveenmaurya09@users.noreply.github.com>
2024-01-15 20:19:12 +0530

    Theme setup & custom theme components added

---
+++ lib/utils/theme/custom_theme/checkbox_theme.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class KCheckBoxTheme {
+  KCheckBoxTheme._();
+
+  /// LightCheckBox Theme
+  static CheckboxThemeData lightCheckboxTheme = CheckboxThemeData(
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(4),
+    ),
+    checkColor: MaterialStateProperty.resolveWith((states) {
+      if (states.contains(MaterialState.selected)){
+        return Colors.white;
+      } else {
+        return Colors.black;
+      }
+    }),
+    fillColor: MaterialStateProperty.resolveWith((states) {
+      if (states.contains(MaterialState.selected)) {
+        return Colors.blue;
+      } else {
+        return Colors.transparent;
+      }
+    })
+  );
+
+
+  /// Dark CheckBox Theme
+  static CheckboxThemeData darkCheckboxTheme = CheckboxThemeData(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(4),
+      ),
+      checkColor: MaterialStateProperty.resolveWith((states) {
+        if (states.contains(MaterialState.selected)){
+          return Colors.white;
+        } else {
+          return Colors.black;
+        }
+      }),
+      fillColor: MaterialStateProperty.resolveWith((states) {
+        if (states.contains(MaterialState.selected)) {
+          return Colors.blue;
+        } else {
+          return Colors.transparent;
+        }
+      })
+  );
+
+
+
+}
--- lib/utils/theme/theme.dart
+++ lib/utils/theme/theme.dart
@@ -1,17 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:our_store/utils/theme/custom_theme/appbar_theme.dart';
+import 'package:our_store/utils/theme/custom_theme/bottom_sheet_theme.dart';
+import 'package:our_store/utils/theme/custom_theme/checkbox_theme.dart';
+import 'package:our_store/utils/theme/custom_theme/chip_theme.dart';
+import 'package:our_store/utils/theme/custom_theme/elevated_button_theme.dart';
+import 'package:our_store/utils/theme/custom_theme/outlined_button_theme.dart';
+import 'package:our_store/utils/theme/custom_theme/text_field_theme.dart';
 import 'package:our_store/utils/theme/custom_theme/text_theme.dart';
 
 class KAppTheme {
   KAppTheme._();
 
+  /// Light Theme & It's custom theme data
   static ThemeData lightTheme = ThemeData(
     useMaterial3: true,
     fontFamily: 'Poppins',
     brightness: Brightness.light,
     primaryColor: Colors.blue,
     scaffoldBackgroundColor: Colors.white,
-    textTheme: KTextTheme.lightTextTheme
+    textTheme: KTextTheme.lightTextTheme,
+    chipTheme: KChipTheme.lightChipTheme,
+    appBarTheme: KAppBarTheme.lightAppBarTheme,
+    checkboxTheme: KCheckBoxTheme.lightCheckboxTheme,
+    bottomSheetTheme: KBottomSheetTheme.lightBottomSheetTheme,
+    elevatedButtonTheme: KElevatedButtonTheme.lightElevatedButtonTheme,
+    outlinedButtonTheme: KOutlinedButtonTheme.lightOutlinedButtonTheme,
+    inputDecorationTheme: KTextFormFieldTheme.lightInputDecorationTheme,
   );
+
+
+  /// Dark Theme & It's custom theme data
   static ThemeData darkTheme = ThemeData(
     useMaterial3: true,
     fontFamily: 'Poppins',
@@ -19,6 +37,13 @@ class KAppTheme {
     primaryColor: Colors.blue,
     scaffoldBackgroundColor: Colors.black,
     textTheme: KTextTheme.darkTextTheme,
+    chipTheme: KChipTheme.darkChipTheme,
+    appBarTheme: KAppBarTheme.darkAppBarTheme,
+    checkboxTheme: KCheckBoxTheme.darkCheckboxTheme,
+    bottomSheetTheme: KBottomSheetTheme.darkBottomSheetTheme,
+    elevatedButtonTheme: KElevatedButtonTheme.darkElevatedButtonTheme,
+    outlinedButtonTheme: KOutlinedButtonTheme.darkOutlinedButtonTheme,
+    inputDecorationTheme: KTextFormFieldTheme.darkInputDecorationTheme
   );
 
 
